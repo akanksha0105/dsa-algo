eea10a8d210a95e6ddbb44b185db74bcf1c48afe
Akanksha <mittalakanksha0105@gmail.com>
2024-08-08 22:33:03 +0530

    contains duplicate

--- Trees/validBST.js
+++ Trees/validBST.js
@@ -1,24 +1,21 @@
-var isValidBST = function(root) {
-    
-    return validate(root, -Infinity, Infinity);
-};
+function helper(root, min, max) {
+	// An empty tree is a BST
+	if (root === null) {
+		return true;
+	}
 
-var validate = function(node, lower,upper){
-    
-    if ( node == null ){
-        
-        // empty node or empty tree
-        return true;
-    }
-    
-    if( (lower < node.val) && ( node.val < upper ) ){
-        
-        // check if all tree nodes follow BST rule
-        return validate( node.left, lower, node.val) && validate( node.right, node.val, upper);
-    }else{
-        
-        // early reject when we find violation
-        return false;
-    }
-    
+	// False if this node violates the min/max constraint
+	if (root.data < min || root.data > max) {
+		return false;
+	}
+
+	// Otherwise, check the subtrees recursively, modifying the min or max constraint
+	const leftSearch = helper(root.left, min, root.data);
+	const rightSearch = helper(root.right, root.data, max);
+
+	return leftSearch && rightSearch;
+}
+
+function validateBST(root) {
+	return helper(root, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
 }
