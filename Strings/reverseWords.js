9f709e677ffe61d1356d3731cca187611976497b
Akanksha <mittalakanksha0105@gmail.com>
2024-08-29 22:33:51 +0530

    reverseWords of string

--- Strings/reverseWords.js
+++ Strings/reverseWords.js
@@ -1,12 +1,15 @@
+//Time complexity : O(n)
+
 function reverseWords(s) {
     // Trim the input string to remove leading and trailing spaces
     // Split the string by one or more spaces into an array of words
     // Reverse the array of words
     // Join the reversed array into a single string with a single space separating each word
+    console.log(s.trim().split(/\s+/).reverse());
     return s.trim().split(/\s+/).reverse().join(' ');
 }
 
 // Example usage
-const input = "the sky is blue";
+const input = "   the sky is blue  ";
 const output = reverseWords(input);
 console.log(output); // Output: "blue is sky the"
