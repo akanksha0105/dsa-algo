aee5d45156b8f019f3a84d52b923666a0f12ad96
Akanksha <mittalakanksha0105@gmail.com>
2024-09-28 11:11:34 +0530

    first unique character of string

--- Arrays/flattenArray.js
+++ Arrays/flattenArray.js
@@ -1,3 +1,5 @@
+
+//Recursive function
 function flattenArray(arr, depth = Infinity) {
     const flattened = [];
     
@@ -22,4 +24,30 @@ function flattenArray(arr, depth = Infinity) {
   
 
 
+  //Iterative solution using stack
+
+
   //depth: levels of nesting
+  //the time complexity is O(n) because every element in the array is visited once and combined via concatenation or spreading.
+
+function flat(arr, depth = 1) {
+  let result = [];
+  let stack = arr.map(el => [el, depth]);
+    console.log("stack", stack)
+  while(stack.length) {
+    const [item, itemDepth] = stack.pop();
+    if(Array.isArray(item) && itemDepth > 0) {
+      stack.push(...item.map(el => [el, itemDepth - 1]));
+      console.log("stack updated", stack)
+    } else {
+      result.push(item)
+    }
+  }
+
+  return result.reverse()
+}
+
+
+const arr = [1, [2], [3, [4]]];
+
+console.log(flat(arr))
