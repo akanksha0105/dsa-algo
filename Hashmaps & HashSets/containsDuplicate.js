08d466fd97b83c6dc457fea1fbdbe3087516ba1f
Akanksha <mittalakanksha0105@gmail.com>
2024-09-28 22:38:05 +0530

    array contains duplicate

--- Hashmaps & HashSets/containsDuplicate.js
+++ Hashmaps & HashSets/containsDuplicate.js
@@ -2,3 +2,17 @@ var containsDuplicate = function (nums) {
 	const s = new Set(nums);
 	return s.size !== nums.length;
 };
+
+
+const repeatedNumber = function(nums){
+	const seen = new Set();
+ 
+	 for (const num of nums) {
+		 if (seen.has(num)) {
+			 return num; // Return the first repeated number
+		 }
+		 seen.add(num);
+	 }
+ 
+	 return -1;
+	 }
